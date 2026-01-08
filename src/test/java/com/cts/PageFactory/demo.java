a3bec90b8917cc5135d7858e413c3e77d839c756
Nandhini27071992 <nandhinin364@gmail.com>
2026-01-09 01:05:20 +0530

    Demofile

--- src/test/java/com/cts/PageFactory/demo.java
+++ src/test/java/com/cts/PageFactory/demo.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demo {
+	public void add() {
+		System.out.println("Addition");
+	}
 	public static void main(String[] args) throws InterruptedException {
-		
+		//Test changes
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.cheapair.com/");
 		driver.manage().window().maximize();
